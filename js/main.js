38850a45f3369d07429dff397e29d3a388d2e0dc
tenderserg <tenderserg@mail.ru>
2017-05-17 18:28:44 +0300

    DZ_js_10

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// (function($, undefined) {
+    
+// })(jQwery);
+
+
+
+//Инициализация карусели плагина Slick
+$('.carousel').slick({
+	slidesToShow : 4,
+	slidesToScroll: 2,
+	prevArrow: '<img class="slick-prev" src="Img/Arrow-prev.png">',	
+  nextArrow: '<img class="slick-next" src="Img/Arrow-next.png">'
+    
+});
+// Инициализация карусели плагина Slick
+
+
+
+
+$(document).ready(function() {
+
+
+});
+
